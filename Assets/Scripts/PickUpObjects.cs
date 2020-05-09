66826fccde80c05712506e558eafd88387b6ba1b
102606361 <102606361@student.swin.edu.au>
2020-05-09 16:27:28 +1000

    PickUpObjectFirst

---
+++ Assets/Scripts/PickUpObjects.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PickUpObjects : MonoBehaviour { 
+    GameObject mainCamera;
+    bool carrying;
+    GameObject carriedObject;
+    public float distance;
+    public float smooth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(carrying)
+        {
+            carry(carriedObject);
+            checkDrop();
+        }
+        else
+        {
+            pickup();
+        }
+    }
+
+    void rotateObject()
+    {
+        carriedObject.transform.Rotate(5, 10, 15);
+    }
+
+    void carry(GameObject o)
+    {
+        o.transform.position = Vector3.Lerp(o.transform.position, mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+    }
+
+    void pickup()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+            Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                PickUp p = hit.collider.GetComponent<PickUp>();
+                if (p != null)
+                {
+                    carrying = true;
+                    carriedObject = p.gameObject;
+                    p.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                }
+            }
+        }
+    }
+
+    void checkDrop()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            dropObject();
+        }
+    }
+
+    void dropObject()
+    {
+        carrying = false;
+        carriedObject.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        carriedObject = null;
+    }
+}
