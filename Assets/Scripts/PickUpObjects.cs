e412874934b71b7cdbcc338cbb6f5998f5227c1b
102606361 <102606361@student.swin.edu.au>
2020-05-22 14:18:37 +1000

    Highlight targetObj

--- Assets/Scripts/PickUpObjects.cs
+++ Assets/Scripts/PickUpObjects.cs
@@ -8,6 +8,9 @@ public class PickUpObjects : MonoBehaviour {
     public float distance;
     public float smooth;
     public LayerMask layer;
+    private Transform targetObj;
+    public Material highlightMat;
+    private Material normalMat;
 
     // Start is called before the first frame update
     void Start()
@@ -27,8 +30,42 @@ public class PickUpObjects : MonoBehaviour {
         {
             pickup();
         }
+        ChangeMaterial();
     }
 
+
+    void ChangeMaterial()
+    {
+        if (targetObj != null)
+        {
+            targetObj.GetComponent<Renderer>().material = normalMat;
+        }
+
+        if (carrying)
+        {
+            return;
+        }
+       
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, distance + 1, layer))
+        {
+            Debug.Log(hit.transform.name);
+            if (hit.transform.CompareTag("Pickupable"))
+            {
+
+                targetObj = hit.transform;
+                normalMat = targetObj.GetComponent<Renderer>().material;
+                targetObj.GetComponent<Renderer>().material = highlightMat;
+
+            }
+        }
+        
+        
+    }
     void rotateObject()
     {
         carriedObject.transform.Rotate(5, 10, 15);
