5766819874f5af608a253ac5be5a59c671f4bb2f
Monique Kuhn <monique.n.kuhn@gmail.com>
2020-05-24 17:38:58 +1000

    CI setup
    Setting up with example pass unit test in resources folder

---
+++ Assets/Tests/CI/NewTestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        // A pass test for CI setup
+        [Test]
+        public void SomeGreenTestPassing()
+        {
+            Assert.Pass();
+        }
+    }
+}
